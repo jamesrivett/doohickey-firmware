a2b40619a4a7f6a5af0db73c5b433a3e55dec30f
jamesrivett <jamesrivett77@gmail.com>
2023-03-27 16:57:25 -0400

    we just out here printing

---
+++ include/pins.h
@@ -0,0 +1,18 @@
+const int BUTTON_0 = 15;
+const int BUTTON_1 = 14;
+const int BUTTON_2 = 16;
+const int BUTTON_3 = 10;
+
+const int ENC_0_BUTTON = 4;
+const int ENC_1_BUTTON = 5;
+const int ENC_2_BUTTON = 8;
+const int ENC_3_BUTTON = 9;
+
+const int ENC_0_CLK = 21;
+const int ENC_0_DT  = 1;
+const int ENC_1_CLK = 20;
+const int ENC_1_DT  = 0;
+const int ENC_2_CLK = 2;
+const int ENC_2_DT  = 3;
+const int ENC_3_CLK = 6;
+const int ENC_3_DT  = 7;
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,33 @@
+#include <pins.h>
 
 #include <Arduino.h>
 #include <Encoder.h>
 #include <HID-Project.h>
 
 
-
 void setup() {
   Serial.begin(9600);
   BootKeyboard.begin();
   Consumer.begin();
+
+  pinMode(BUTTON_0, INPUT);
+  pinMode(BUTTON_1, INPUT);
+  pinMode(BUTTON_2, INPUT);
+  pinMode(BUTTON_3, INPUT);
+  pinMode(ENC_0_BUTTON, INPUT_PULLUP);
+  pinMode(ENC_1_BUTTON, INPUT_PULLUP);
+  pinMode(ENC_2_BUTTON, INPUT_PULLUP);
+  pinMode(ENC_3_BUTTON, INPUT_PULLUP);
 }
 
 void loop() {
-
+  Serial.println(digitalRead(BUTTON_0));
+  Serial.println(digitalRead(BUTTON_1));
+  Serial.println(digitalRead(BUTTON_2));
+  Serial.println(digitalRead(BUTTON_3));
+  Serial.println(digitalRead(ENC_0_BUTTON));
+  Serial.println(digitalRead(ENC_1_BUTTON));
+  Serial.println(digitalRead(ENC_2_BUTTON));
+  Serial.println(digitalRead(ENC_3_BUTTON));
+  delay(50);
 }
