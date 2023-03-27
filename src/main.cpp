44c115bed82832c262777fcf90105891135a5648
jamesrivett <jamesrivett77@gmail.com>
2023-03-27 19:40:38 -0400

    reading all values

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,7 @@
 #include <Encoder.h>
 #include <HID-Project.h>
 
-
-void setup() {
-  Serial.begin(9600);
+void setup() { Serial.begin(9600);
   BootKeyboard.begin();
   Consumer.begin();
 
@@ -20,6 +18,11 @@ void setup() {
   pinMode(ENC_3_BUTTON, INPUT_PULLUP);
 }
 
+Encoder ENC_0 = Encoder(ENC_0_CLK, ENC_0_DT);
+Encoder ENC_1 = Encoder(ENC_1_CLK, ENC_1_DT);
+Encoder ENC_2 = Encoder(ENC_2_CLK, ENC_2_DT);
+Encoder ENC_3 = Encoder(ENC_3_CLK, ENC_3_DT);
+
 void loop() {
   Serial.println(digitalRead(BUTTON_0));
   Serial.println(digitalRead(BUTTON_1));
@@ -29,5 +32,9 @@ void loop() {
   Serial.println(digitalRead(ENC_1_BUTTON));
   Serial.println(digitalRead(ENC_2_BUTTON));
   Serial.println(digitalRead(ENC_3_BUTTON));
+  Serial.println(ENC_0.read());
+  Serial.println(ENC_1.read());
+  Serial.println(ENC_2.read());
+  Serial.println(ENC_3.read());
   delay(50);
 }
