3663e15a4ec980d52ac93917400d6110acc5801b
jamesrivett <jamesrivett77@gmail.com>
2023-04-11 21:22:21 -0400

    slight format

--- src/actions.cpp
+++ src/actions.cpp
@@ -6,232 +6,231 @@
 #include "actions.hpp"
 #include "state.hpp"
 
-
 void doohickeyInit() {
-    Serial.begin(9600);
-    BootKeyboard.begin();
-    Consumer.begin();
+  Serial.begin(9600);
+  BootKeyboard.begin();
+  Consumer.begin();
 
-    pinMode(BUTTON_0, INPUT);
-    pinMode(BUTTON_1, INPUT);
-    pinMode(BUTTON_2, INPUT);
-    pinMode(BUTTON_3, INPUT);
-    pinMode(ENC_0_BUTTON, INPUT_PULLUP);
-    pinMode(ENC_1_BUTTON, INPUT_PULLUP);
-    pinMode(ENC_2_BUTTON, INPUT_PULLUP);
-    pinMode(ENC_3_BUTTON, INPUT_PULLUP);
+  pinMode(BUTTON_0, INPUT);
+  pinMode(BUTTON_1, INPUT);
+  pinMode(BUTTON_2, INPUT);
+  pinMode(BUTTON_3, INPUT);
+  pinMode(ENC_0_BUTTON, INPUT_PULLUP);
+  pinMode(ENC_1_BUTTON, INPUT_PULLUP);
+  pinMode(ENC_2_BUTTON, INPUT_PULLUP);
+  pinMode(ENC_3_BUTTON, INPUT_PULLUP);
 }
 
 void BUTTON_0_PRESS() {
-    if (SHIFT) {
-      Consumer.write(HID_CONSUMER_AC_FORWARD);
-    }
-    else {
-      Consumer.write(MEDIA_NEXT);
-    }
+  if (SHIFT) {
+    Consumer.write(HID_CONSUMER_AC_FORWARD);
+  }
+  else {
+    Consumer.write(MEDIA_NEXT);
+  }
 }
 
 void BUTTON_1_PRESS() {
-    if (SHIFT) {
-    }
-    else {
-      Consumer.write(MEDIA_PLAY_PAUSE);
-    }
+  if (SHIFT) {
+  }
+  else {
+    Consumer.write(MEDIA_PLAY_PAUSE);
+  }
 }
 
 void BUTTON_2_PRESS() {
-    if (SHIFT) {
-      Consumer.write(HID_CONSUMER_AC_BACK);
-    }
-    else {
-      Consumer.write(MEDIA_PREVIOUS);
-    }
+  if (SHIFT) {
+    Consumer.write(HID_CONSUMER_AC_BACK);
+  }
+  else {
+    Consumer.write(MEDIA_PREVIOUS);
+  }
 }
 
 void BUTTON_ENC_0_PRESS() {
-    switch(MODE) {
-        case 0:
-        Consumer.write(MEDIA_VOL_MUTE);
-        break;
-        case 1:
-        if (SHIFT) {
-            BootKeyboard.press(KEY_RIGHT_ALT);
-            BootKeyboard.write(KEY_RIGHT_BRACE);
-            BootKeyboard.release(KEY_RIGHT_ALT);
-        }
-        else {
-            BootKeyboard.press(KEY_RIGHT_ALT);
-            BootKeyboard.write(KEY_LEFT_BRACE);
-            BootKeyboard.release(KEY_RIGHT_ALT);
-        }
-        break;
+  switch (MODE) {
+  case 0:
+    Consumer.write(MEDIA_VOL_MUTE);
+    break;
+  case 1:
+    if (SHIFT) {
+      BootKeyboard.press(KEY_RIGHT_ALT);
+      BootKeyboard.write(KEY_RIGHT_BRACE);
+      BootKeyboard.release(KEY_RIGHT_ALT);
     }
+    else {
+      BootKeyboard.press(KEY_RIGHT_ALT);
+      BootKeyboard.write(KEY_LEFT_BRACE);
+      BootKeyboard.release(KEY_RIGHT_ALT);
+    }
+    break;
+  }
 }
 
 void BUTTON_ENC_1_PRESS() {
-    switch(MODE) {
-      case 0:
-        BootKeyboard.write(KEY_ENTER);
-        break;
-    }
+  switch (MODE) {
+  case 0:
+    BootKeyboard.write(KEY_ENTER);
+    break;
+  }
 }
 
 void BUTTON_ENC_2_PRESS() {
-    switch(MODE) {
-      case 1:
-        BootKeyboard.write(KEY_ENTER);
-        break;
-    }
+  switch (MODE) {
+  case 1:
+    BootKeyboard.write(KEY_ENTER);
+    break;
+  }
 }
 
 void BUTTON_ENC_3_PRESS() {
-    switch(MODE) {
-      case 0:
-        if (SHIFT) {
-          BootKeyboard.press(KEY_RIGHT_ALT);
-          BootKeyboard.write(KEY_RIGHT_BRACE);
-          BootKeyboard.release(KEY_RIGHT_ALT);
-        }
-        else {
-          BootKeyboard.press(KEY_RIGHT_ALT);
-          BootKeyboard.write(KEY_LEFT_BRACE);
-          BootKeyboard.release(KEY_RIGHT_ALT);
-        }
-        break;
-      case 1:
-        Consumer.write(MEDIA_VOL_MUTE);
-        break;
-      case 2:
-        BootKeyboard.press(KEY_SPACE);
-        break;
+  switch (MODE) {
+  case 0:
+    if (SHIFT) {
+      BootKeyboard.press(KEY_RIGHT_ALT);
+      BootKeyboard.write(KEY_RIGHT_BRACE);
+      BootKeyboard.release(KEY_RIGHT_ALT);
     }
+    else {
+      BootKeyboard.press(KEY_RIGHT_ALT);
+      BootKeyboard.write(KEY_LEFT_BRACE);
+      BootKeyboard.release(KEY_RIGHT_ALT);
+    }
+    break;
+  case 1:
+    Consumer.write(MEDIA_VOL_MUTE);
+    break;
+  case 2:
+    BootKeyboard.press(KEY_SPACE);
+    break;
+  }
 }
 
 void BUTTON_ENC_3_RELEASE() {
-    switch(MODE) {
-      case 2:
-        BootKeyboard.release(KEY_SPACE);
-    }
+  switch (MODE) {
+  case 2:
+    BootKeyboard.release(KEY_SPACE);
+  }
 }
 
 void ENC_0_LEFT() {
-    switch(MODE) {
-      case 0:
-        Consumer.write(MEDIA_VOL_DOWN);
-        break;
-      case 2:
-        BootKeyboard.write(KEY_LEFT_BRACE);
-        break;
-    }
+  switch (MODE) {
+  case 0:
+    Consumer.write(MEDIA_VOL_DOWN);
+    break;
+  case 2:
+    BootKeyboard.write(KEY_LEFT_BRACE);
+    break;
+  }
 }
 void ENC_0_RIGHT() {
-    switch(MODE) {
-      case 0:
-        Consumer.write(MEDIA_VOL_UP);
-        break;
-      case 2:
-        BootKeyboard.write(KEY_RIGHT_BRACE);
-        break;
-    }
+  switch (MODE) {
+  case 0:
+    Consumer.write(MEDIA_VOL_UP);
+    break;
+  case 2:
+    BootKeyboard.write(KEY_RIGHT_BRACE);
+    break;
+  }
 }
 
 void ENC_1_LEFT() {
-    switch(MODE) {
-      case 0:
-        if (SHIFT) {
-          SCROLLING_SPEED = constrain(SCROLLING_SPEED-1, 1, 5);
-          scrollBlink(SCROLLING_SPEED);
-        }
-        else {
-          for (int i = 0; i < SCROLLING_SPEED; i++) {
-            BootKeyboard.write(KEY_DOWN_ARROW);
-          }
-        }
-        break;
-      case 1:
-        BootKeyboard.write(KEY_LEFT_ARROW);
-        break;
+  switch (MODE) {
+  case 0:
+    if (SHIFT) {
+      SCROLLING_SPEED = constrain(SCROLLING_SPEED - 1, 1, 5);
+      scrollBlink(SCROLLING_SPEED);
     }
+    else {
+      for (int i = 0; i < SCROLLING_SPEED; i++) {
+        BootKeyboard.write(KEY_DOWN_ARROW);
+      }
+    }
+    break;
+  case 1:
+    BootKeyboard.write(KEY_LEFT_ARROW);
+    break;
+  }
 }
 void ENC_1_RIGHT() {
-    switch(MODE) {
-      case 0:
-        if (SHIFT) {
-          SCROLLING_SPEED = constrain(SCROLLING_SPEED+1, 1, 5);
-          scrollBlink(SCROLLING_SPEED);
-        }
-        else {
-          for (int i = 0; i < SCROLLING_SPEED; i++) {
-            BootKeyboard.write(KEY_UP_ARROW);
-          }
-        }
-        break;
-      case 1:
-        BootKeyboard.write(KEY_RIGHT_ARROW);
-        break;
+  switch (MODE) {
+  case 0:
+    if (SHIFT) {
+      SCROLLING_SPEED = constrain(SCROLLING_SPEED + 1, 1, 5);
+      scrollBlink(SCROLLING_SPEED);
     }
+    else {
+      for (int i = 0; i < SCROLLING_SPEED; i++) {
+        BootKeyboard.write(KEY_UP_ARROW);
+      }
+    }
+    break;
+  case 1:
+    BootKeyboard.write(KEY_RIGHT_ARROW);
+    break;
+  }
 }
 
 void ENC_2_LEFT() {
- switch(MODE) {
-      case 0:
-        BootKeyboard.write(KEY_LEFT_ARROW);
-        break;
-      case 1:
-        if (SHIFT) {
-          SCROLLING_SPEED = constrain(SCROLLING_SPEED-1, 1, 5);
-          scrollBlink(SCROLLING_SPEED);
-        }
-        else {
-          for (int i = 0; i < SCROLLING_SPEED; i++) {
-            BootKeyboard.write(KEY_DOWN_ARROW);
-          }
-        }
-        break;
+  switch (MODE) {
+  case 0:
+    BootKeyboard.write(KEY_LEFT_ARROW);
+    break;
+  case 1:
+    if (SHIFT) {
+      SCROLLING_SPEED = constrain(SCROLLING_SPEED - 1, 1, 5);
+      scrollBlink(SCROLLING_SPEED);
     }
+    else {
+      for (int i = 0; i < SCROLLING_SPEED; i++) {
+        BootKeyboard.write(KEY_DOWN_ARROW);
+      }
+    }
+    break;
+  }
 }
 void ENC_2_RIGHT() {
-switch(MODE) {
-      case 0:
-        BootKeyboard.write(KEY_RIGHT_ARROW);
-        break;
-      case 1:
-        if (SHIFT) {
-          SCROLLING_SPEED = constrain(SCROLLING_SPEED+1, 1, 5);
-          scrollBlink(SCROLLING_SPEED);
-        }
-        else {
-          for (int i = 0; i < SCROLLING_SPEED; i++) {
-            BootKeyboard.write(KEY_UP_ARROW);
-          }
-        }
-        break;
+  switch (MODE) {
+  case 0:
+    BootKeyboard.write(KEY_RIGHT_ARROW);
+    break;
+  case 1:
+    if (SHIFT) {
+      SCROLLING_SPEED = constrain(SCROLLING_SPEED + 1, 1, 5);
+      scrollBlink(SCROLLING_SPEED);
     }
+    else {
+      for (int i = 0; i < SCROLLING_SPEED; i++) {
+        BootKeyboard.write(KEY_UP_ARROW);
+      }
+    }
+    break;
+  }
 }
 
 void ENC_3_LEFT() {
- switch(MODE) {
-      case 1:
-        Consumer.write(MEDIA_VOL_DOWN);
-        break;
-      case 2:
-        BootKeyboard.press(KEY_LEFT_CTRL);
-        BootKeyboard.write(HID_KEYBOARD_MINUS_AND_UNDERSCORE);
-        BootKeyboard.releaseAll();
-        break;
-    }
+  switch (MODE) {
+  case 1:
+    Consumer.write(MEDIA_VOL_DOWN);
+    break;
+  case 2:
+    BootKeyboard.press(KEY_LEFT_CTRL);
+    BootKeyboard.write(HID_KEYBOARD_MINUS_AND_UNDERSCORE);
+    BootKeyboard.releaseAll();
+    break;
+  }
 }
 void ENC_3_RIGHT() {
-  switch(MODE) {
-      case 1:
-        Consumer.write(MEDIA_VOL_UP);
-        break;
-      case 2:
-        BootKeyboard.press(KEY_LEFT_CTRL);
-        BootKeyboard.write(HID_KEYBOARD_EQUALS_AND_PLUS);
-        BootKeyboard.releaseAll();
-        break;
-    }
+  switch (MODE) {
+  case 1:
+    Consumer.write(MEDIA_VOL_UP);
+    break;
+  case 2:
+    BootKeyboard.press(KEY_LEFT_CTRL);
+    BootKeyboard.write(HID_KEYBOARD_EQUALS_AND_PLUS);
+    BootKeyboard.releaseAll();
+    break;
+  }
 }
 
 void modeBlink(int mode) {
@@ -244,7 +243,7 @@ void modeBlink(int mode) {
   TXLED0;
   delay(50);
 
-  for(u8 i = 0; i < (mode + 1); i++) {
+  for (u8 i = 0; i < (mode + 1); i++) {
     TXLED1;
     delay(300);
     TXLED0;
@@ -253,7 +252,7 @@ void modeBlink(int mode) {
 }
 
 void scrollBlink(int scrolling_speed) {
-  for(int i = 0; i < 3; i++) {
+  for (int i = 0; i < 3; i++) {
     delay(200 / scrolling_speed);
     TXLED1;
     delay(200 / scrolling_speed);
@@ -303,14 +302,14 @@ bool checkForModeChange() {
 }
 
 void handleModeChange() {
-    if (currentStates[8] > previousStates[8]) {
-      MODE = constrain(MODE + 1, 0, NUM_MODES - 1);
-    }
-    else if (currentStates[8] < previousStates[8]) {
-      MODE = constrain(MODE - 1, 0, NUM_MODES - 1);
-    }
-    modeBlink(MODE);
-    lastModeChange = millis();
-    updatePreviousStates();
-    return;
+  if (currentStates[8] > previousStates[8]) {
+    MODE = constrain(MODE + 1, 0, NUM_MODES - 1);
+  }
+  else if (currentStates[8] < previousStates[8]) {
+    MODE = constrain(MODE - 1, 0, NUM_MODES - 1);
+  }
+  modeBlink(MODE);
+  lastModeChange = millis();
+  updatePreviousStates();
+  return;
 }
