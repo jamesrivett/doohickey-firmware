82da2c2d7f7ca6956086471545ec3c8a8434e822
jamesrivett <61210134+jamesrivett@users.noreply.github.com>
2024-03-16 14:41:44 -0400

    finishing struct driven input states

--- lib/state/state.cpp
+++ lib/state/state.cpp
@@ -14,66 +14,51 @@ long timeSinceLastModeChange = 0;
 
 ModeState modeState;
 
-struct InputStates {
-  bool state_BUTTON_0;
-  bool state_BUTTON_1;
-  bool state_BUTTON_2;
-  bool state_BUTTON_3;
-  bool state_ENC_0_BUTTON;
-  bool state_ENC_1_BUTTON;
-  bool state_ENC_2_BUTTON;
-  bool state_ENC_3_BUTTON;
-  int state_ENC_0;
-  int state_ENC_1;
-  int state_ENC_2;
-  int state_ENC_3;
-};
-
 InputStates currentStates;
 InputStates previousStates;
 
 void updateCurrentStates() {
-  currentStates.state_BUTTON_0 = digitalRead(BUTTON_0);
-  currentStates.state_BUTTON_1 = digitalRead(BUTTON_1);
-  currentStates.state_BUTTON_2 = digitalRead(BUTTON_2);
-  currentStates.state_BUTTON_3 = digitalRead(BUTTON_3);
-  currentStates.state_ENC_0_BUTTON = !digitalRead(ENC_0_BUTTON);
-  currentStates.state_ENC_1_BUTTON = !digitalRead(ENC_1_BUTTON);
-  currentStates.state_ENC_2_BUTTON = !digitalRead(ENC_2_BUTTON);
-  currentStates.state_ENC_3_BUTTON = !digitalRead(ENC_3_BUTTON);
+  currentStates.BUTTON_0 = digitalRead(BUTTON_0);
+  currentStates.BUTTON_1 = digitalRead(BUTTON_1);
+  currentStates.BUTTON_2 = digitalRead(BUTTON_2);
+  currentStates.BUTTON_3 = digitalRead(BUTTON_3);
+  currentStates.ENC_0_BUTTON = !digitalRead(ENC_0_BUTTON);
+  currentStates.ENC_1_BUTTON = !digitalRead(ENC_1_BUTTON);
+  currentStates.ENC_2_BUTTON = !digitalRead(ENC_2_BUTTON);
+  currentStates.ENC_3_BUTTON = !digitalRead(ENC_3_BUTTON);
   noInterrupts();
-  currentStates.state_ENC_0 = (-1 * ENC_0.read()) / 4;
-  currentStates.state_ENC_1 = (-1 * ENC_1.read()) / 4;
-  currentStates.state_ENC_2 = ENC_2.read() / 4;
-  currentStates.state_ENC_3 = ENC_3.read() / 4;
+  currentStates.ENC_0 = (-1 * ENC_0.read()) / 4;
+  currentStates.ENC_1 = (-1 * ENC_1.read()) / 4;
+  currentStates.ENC_2 = ENC_2.read() / 4;
+  currentStates.ENC_3 = ENC_3.read() / 4;
   interrupts();
 }
 
 void updatePreviousStates() {
-  currentStates.state_BUTTON_0 = previousStates.state_BUTTON_0;
-  currentStates.state_BUTTON_1 = previousStates.state_BUTTON_1;
-  currentStates.state_BUTTON_2 = previousStates.state_BUTTON_2;
-  currentStates.state_BUTTON_3 = previousStates.state_BUTTON_3;
-  currentStates.state_ENC_0_BUTTON = previousStates.state_ENC_0_BUTTON;
-  currentStates.state_ENC_1_BUTTON = previousStates.state_ENC_1_BUTTON;
-  currentStates.state_ENC_2_BUTTON = previousStates.state_ENC_2_BUTTON;
-  currentStates.state_ENC_3_BUTTON = previousStates.state_ENC_3_BUTTON;
-  currentStates.state_ENC_0 = previousStates.state_ENC_0;
-  currentStates.state_ENC_1 = previousStates.state_ENC_1;
-  currentStates.state_ENC_2 = previousStates.state_ENC_2;
-  currentStates.state_ENC_3 = previousStates.state_ENC_3;
+  previousStates.BUTTON_0 = currentStates.BUTTON_0;
+  previousStates.BUTTON_1 = currentStates.BUTTON_1;
+  previousStates.BUTTON_2 = currentStates.BUTTON_2;
+  previousStates.BUTTON_3 = currentStates.BUTTON_3;
+  previousStates.ENC_0_BUTTON = currentStates.ENC_0_BUTTON;
+  previousStates.ENC_1_BUTTON = currentStates.ENC_1_BUTTON;
+  previousStates.ENC_2_BUTTON = currentStates.ENC_2_BUTTON;
+  previousStates.ENC_3_BUTTON = currentStates.ENC_3_BUTTON;
+  previousStates.ENC_0 = currentStates.ENC_0;
+  previousStates.ENC_1 = currentStates.ENC_1;
+  previousStates.ENC_2 = currentStates.ENC_2;
+  previousStates.ENC_3 = currentStates.ENC_3;
 }
 
 bool checkForModeChange() {
   timeSinceLastModeChange = millis() - lastModeChange;
-  return currentStates[3] && currentStates[8] != previousStates[8] && timeSinceLastModeChange > 300;
+  return currentStates.BUTTON_3 && currentStates.ENC_0 != previousStates.ENC_0 && timeSinceLastModeChange > 300;
 }
 
 void handleModeChange() {
-  if (currentStates[8] > previousStates[8]) {
+  if (currentStates.ENC_0 > previousStates.ENC_0) {
     modeState = ModeState(constrain(modeState + 1, 0, NUM_MODES - 1));
   }
-  else if (currentStates[8] < previousStates[8]) {
+  else if (currentStates.ENC_0 < previousStates.ENC_0) {
     modeState = ModeState(constrain(modeState - 1, 0, NUM_MODES - 1));
   }
   modeBlink(modeState);
