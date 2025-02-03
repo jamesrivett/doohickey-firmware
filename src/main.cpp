d2df7ca2f78fb95e553e232cc9325d8aa69a8216
jamesrivett <61210134+jamesrivett@users.noreply.github.com>
2025-02-02 22:48:39 -0500

    changing debug

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ void loop() {
   long t1 = micros();
   int delta = t1 - t0;
   float deltaMillis = float(delta) / 1000;
-  // Serial.println("delta: " + String(deltaMillis) + " miliseconds");
+  Serial.println("delta: " + String(deltaMillis) + " miliseconds");
   doohickeyDebug();
   delayMicroseconds(10000 - delta);
 }
