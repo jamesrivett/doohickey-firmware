5ce5e6a883c38de7689a954e37317b10228826c0
jamesrivett <61210134+jamesrivett@users.noreply.github.com>
2025-02-02 22:48:39 -0500

    changing debug

--- lib/actions/actions.cpp
+++ lib/actions/actions.cpp
@@ -131,16 +131,17 @@ void scrollBlink(int scrolling_speed) {
 }
 
 void doohickeyDebug() {
-  Serial.println("button0: " + String(currentStates.BUTTON_0));
-  Serial.println("button1: " + String(currentStates.BUTTON_1));
-  Serial.println("button2: " + String(currentStates.BUTTON_2));
-  Serial.println("button3: " + String(currentStates.BUTTON_3));
-  Serial.println("enc_button0: " + String(currentStates.ENC_0_BUTTON));
-  Serial.println("enc_button1: " + String(currentStates.ENC_1_BUTTON));
-  Serial.println("enc_button2: " + String(currentStates.ENC_2_BUTTON));
-  Serial.println("enc_button3: " + String(currentStates.ENC_3_BUTTON));
+  // Serial.println("button0: " + String(currentStates.BUTTON_0));
+  // Serial.println("button1: " + String(currentStates.BUTTON_1));
+  // Serial.println("button2: " + String(currentStates.BUTTON_2));
+  // Serial.println("button3: " + String(currentStates.BUTTON_3));
+  // Serial.println("enc_button0: " + String(currentStates.ENC_0_BUTTON));
+  // Serial.println("enc_button1: " + String(currentStates.ENC_1_BUTTON));
+  // Serial.println("enc_button2: " + String(currentStates.ENC_2_BUTTON));
+  // Serial.println("enc_button3: " + String(currentStates.ENC_3_BUTTON));
   Serial.println("enc_0: " + String(currentStates.ENC_0));
   Serial.println("enc_1: " + String(currentStates.ENC_1));
   Serial.println("enc_2: " + String(currentStates.ENC_2));
   Serial.println("enc_3: " + String(currentStates.ENC_3));
+  Serial.println("\n\n\n\n\n\n");
 }
